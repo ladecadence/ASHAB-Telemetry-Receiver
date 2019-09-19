48360142b67011969128b4127121629a1facc20e
David Pello <dpello@ladecadence.net>
2019-09-19 19:13:30 +0200

    web interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #define SERIAL_RX_BUFFER_SIZE 255
 
 #include "Arduino.h"
+#include <inttypes.h>
 #define U8G2_USE_LARGE_FONTS
 #include <U8g2lib.h>
 #include <SPI.h>
 #include <RH_RF95.h>
 #include "logo.xbm"
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
 
 #define DEBUG_SERIAL Serial
 
@@ -41,21 +47,105 @@
 // Objects
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ OLED_CLK, /* data=*/ OLED_DAT, /* reset=*/ OLED_RST);
 RH_RF95 rf95(LORA_SS, LORA_INT);
+WiFiServer server(80);
 
 // variables
 uint8_t pkt_start[4] = {0xAA, 0x55, 0xAA, 0x55};
 uint8_t pkt_end[4] = {0x33, 0xCC, 0x33, 0xCC};
 
+
+const char* page_top =
+"<html lang='en'>\n"
+"	<head>\n"
+"		<meta charset='utf-8'>\n"
+"		<title>EKI Telemetry</title>\n"
+"		<style>\n"
+"			body {\n"
+"				font-family: sans-serif;\n"
+"				color: gray;\n"
+"				font-size: 3em;\n"
+"			}\n"
+"		</style>\n"
+"	</head>\n"
+"\n"
+"	<body>\n"
+"		<h1>EKI Telemetry</h1>\n"
+"		<span id='telemetry'>";
+
+const char* page_bottom =
+"</span>\n"
+"		<script>\n"
+"			// get data\n"
+"			var telemetry = document.getElementById('telemetry').innerHTML;\n"
+"			if (!telemetry.includes('NO TELEMETRY YET')) {\n"
+"			// clear it\n"
+"				document.getElementById('telemetry').innerHTML = '';\n"
+"\n"
+"				var fields = telemetry.split('/');\n"
+"				var date = fields[8];\n"
+"				document.getElementById('telemetry').innerHTML += 'Date: ' +\n"
+"					date + '<br />\\n';\n"
+"				var time = fields[9];\n"
+"				document.getElementById('telemetry').innerHTML += 'Time: ' +\n"
+"					time + '<br />\\n';\n"
+"				var position = fields[10].split('=')[1];\n"
+"				var lat = position.split(',')[0];\n"
+"				var lon = position.split(',')[1];\n"
+"				document.getElementById('telemetry').innerHTML += 'Position: ' +\n"
+"					\"<a href='http://maps.google.com/maps?z=14&t=m&q=loc:\" +\n"
+"					lat + '+' + lon + \"'> \" + position + \"</a> <br />\\n\";\n"
+"				var altitude = fields[3].split('=')[1];\n"
+"				document.getElementById('telemetry').innerHTML += 'Altitude: ' +\n"
+"					altitude + ' m <br />\\n';\n"
+"				var speed = fields[2];\n"
+"				document.getElementById('telemetry').innerHTML += 'Speed: ' +\n"
+"					altitude + ' kn <br />\\n';\n"
+"				var heading = fields[1].split('O')[1];\n"
+"				document.getElementById('telemetry').innerHTML += 'Heading: ' +\n"
+"					heading + ' º <br />\\n';\n"
+"				var bat = fields[4];\n"
+"				document.getElementById('telemetry').innerHTML += 'Battery: ' +\n"
+"					altitude + ' V <br />\\n';\n"
+"			}\n"
+"		</script>\n"
+"	</body>\n"
+"</html>\n";
+
+
+// wifi
+char ssid [9] = "ASHAB001";
+char* password = "hirikizadi";
+
 // lora receive buffer
 uint8_t lora_buf[LORA_LEN];
+uint8_t lora_len = sizeof(lora_buf);
 // Serial buffer
 uint8_t serial_buf[SERIAL_LEN];
+
+// telemetry buffer (wifi)
+uint8_t last_telem_buf[LORA_LEN];
+uint8_t telem_len = 0;
+
+// packet counter
+uint16_t pkt_counter = 0;
 
 void setup()
 {
 
 	// initialize LED digital pin as an output.
 	pinMode(LED, OUTPUT);
+
+	// WiFi
+    WiFi.softAP(ssid, password);
+    IPAddress myIP = WiFi.softAPIP();
+
+    // and server
+    server.begin();
+
+    // prepare telemetry
+    memset(last_telem_buf, 0, sizeof(last_telem_buf));
+    memcpy(last_telem_buf, "NO TELEMETRY YET", 16);
+    telem_len = 16;
 
 	// init SPI (ESP32)
 	pinMode(18, OUTPUT);
@@ -70,9 +160,10 @@ void setup()
 	// simple interface
 	u8g2.clearBuffer();
 	u8g2.setFont(FONT_7t);
-	u8g2.drawStr(2, LINE8_0,"ASHAB Receiver");
-	u8g2.drawStr(2, LINE8_3,"Last RSSI:");
-	u8g2.drawStr(2, LINE8_5,"Last Len:");
+	u8g2.drawStr(2, LINE8_0, "ASHAB Receiver");
+	u8g2.drawStr(2, LINE8_4, "Packets:");
+	u8g2.drawStr(2, LINE8_5, "Last RSSI:");
+	u8g2.drawStr(2, LINE8_6, "Last Len:");
 	u8g2.sendBuffer();
 
 	// Serial port 
@@ -90,6 +181,7 @@ void setup()
 	{
 		DEBUG_SERIAL.println("#RF95 Init OK");  
 		u8g2.drawStr(2, LINE8_1,"LoRa OK");
+		u8g2.drawStr(2, LINE8_2, myIP.toString().c_str());
 		u8g2.sendBuffer();
 	}
 
@@ -105,32 +197,46 @@ void loop()
 {
 	if (rf95.available())
 	{
+	    lora_len = sizeof(lora_buf);
 		// Should be a message for us now   
-		uint8_t len = sizeof(lora_buf);
-		if (rf95.recv(lora_buf, &len))
+		if (rf95.recv(lora_buf, &lora_len))
 		{
 			// packet received
 			digitalWrite(LED, HIGH);
 
+			// check for telemetry
+			if (lora_buf[0] == '$' && lora_buf[1] == '$') {
+			    // clear
+			    memset(last_telem_buf, 0, sizeof(last_telem_buf));
+			    memcpy(last_telem_buf, lora_buf, lora_len);
+			    telem_len = lora_len;
+			}
+
 			// create serial packet
 			memcpy(serial_buf, pkt_start, 4);
-			memcpy(serial_buf+4, lora_buf, len);
-			memcpy(serial_buf+4+len, pkt_end, 4);
+			memcpy(serial_buf+4, lora_buf, lora_len);
+			memcpy(serial_buf+4+lora_len, pkt_end, 4);
 
 			// send it through serial port
-			DEBUG_SERIAL.write(serial_buf, len+8);
+			DEBUG_SERIAL.write(serial_buf, lora_len+8);
 			digitalWrite(LED, LOW);
 			// clear buffer
 			memset(serial_buf, 0, sizeof(serial_buf));
 
+            // update packet number
+            pkt_counter++;
+            u8g2.drawStr(2+8*11, LINE8_4, "         ");
+            u8g2.setCursor(2+8*11, LINE8_4);
+            u8g2.print(pkt_counter);
+
 			// show RSSI
-			u8g2.drawStr(2, LINE8_4, "           ");
-			u8g2.setCursor(2, LINE8_4);
+			u8g2.drawStr(2+8*11, LINE8_5, "         ");
+			u8g2.setCursor(2+8*11, LINE8_5);
 			u8g2.print(rf95.lastRssi());
 			// show len
-			u8g2.drawStr(2, LINE8_6, "           ");
-			u8g2.setCursor(2, LINE8_6);
-			u8g2.print(len);
+			u8g2.drawStr(2+8*11, LINE8_6, "         ");
+			u8g2.setCursor(2+8*11, LINE8_6);
+			u8g2.print(lora_len);
 			u8g2.sendBuffer();
 
 		}
@@ -140,5 +246,27 @@ void loop()
 			DEBUG_SERIAL.println("#Recv failed");
 		}
 	}
+
+	// check for wifi clients
+	WiFiClient client = server.available();   // listen for incoming clients
+
+    if (client) {                             // if you get a client
+        while (client.connected()) {
+            while (client.available()) {             // if there's bytes to read from the client,
+                client.read();
+            }
+
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html; charset=utf-8");
+            client.println();
+            client.println(page_top);
+            client.write(last_telem_buf, telem_len);
+            client.println(page_bottom);
+
+            client.println();
+            break;
+        }
+        client.stop();
+    }
 
 }
